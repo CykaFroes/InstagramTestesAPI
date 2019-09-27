3e9e1204c512fa613266c5de79c95cfd42c10d34
CykaFroes <nikowluiz@gmail.com>
2019-09-27 04:06:56 -0300

    funciona mano

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="elfsight-app-22b4c311-ed34-4a1d-9044-4e3227fbb20d"></div>
+    <div id="teste"></div>
+    <!-- LightWidget WIDGET --><iframe src="//lightwidget.com/widgets/b663cb27d0745732911169bb59767caa.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+    <div id="instafeed"></div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
